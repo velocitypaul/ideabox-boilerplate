fdb56b47e72e106898195d2740b7ea80d2f12dbd
velocitypaul <68206388+velocitypaul@users.noreply.github.com>
2021-02-09 21:35:31 -0700

    Add /src/main.js to this PR

---
+++ src/main.js
@@ -0,0 +1,29 @@
+let savedIdeas = [];
+
+function saveToStorage() {}
+
+function deleteFromStorage() {}
+
+function deleteIdeaElement(element) {
+  var parent = this.closest(".idea");
+  console.log(parent);
+  parent.remove();
+}
+
+function addStarredClass(element) {
+  var parent = this.closest(".idea");
+  console.log(parent);
+  parent.classList.toggle("is-favorite");
+}
+
+window.onload = function () {
+  var closeButtons = document.querySelectorAll(".idea__delete");
+  closeButtons.forEach(function (element) {
+    element.addEventListener("click", deleteIdeaElement);
+  });
+
+  var starButtons = document.querySelectorAll(".idea__star");
+  starButtons.forEach(function (element) {
+    element.addEventListener("click", addStarredClass);
+  });
+};
